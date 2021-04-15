6e9459663d6f0102825370a0ee94eed6496c9cbd
Marek Marian Pierzchala <marek.pierzchala@statnett.no>
2021-04-15 12:53:04 +0200

    added forecast

--- stand.py
+++ stand.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import pandas as pd
+import matplotlib.pyplot as plt
 
 dstan={
      'plot_nr': 126,
@@ -22,28 +23,70 @@ dstan={
 }
 
 class Stand():
-    def __init__(self,stand,year):
+    def __init__(self,stand):
         #wczytuje tablice ale tu musi byc jakas logika zeby wczytywacodpowiednia tablice
         self.tablica=pd.read_csv('tab.csv')
         try:
-            self.current_age=self.calculate_current_age(stand['year_of_inventory'],stand['age'],year)
-            print(self.tablica.loc[self.tablica['Age']==self.current_age]['LargeTimber'].values[0])
+            self.current_age=self.calculate_current_age(stand['year_of_inventory'],stand['age'])
         except:
             print("Wiek wykracza poza zakres tabeli, uzywam wieku poczatkowego !!!")
             self.current_age=stand['age']
+        self.area=stand['area']
+        self.address=stand['cadastral_address']
         self.current_large_timber=float(self.tablica.loc[self.tablica['Age']==self.current_age]['LargeTimber'].values[0].replace(',','.'))
         self.current_small_timber=float(self.tablica.loc[self.tablica['Age']==self.current_age]['SmallTimber'].values[0].replace(',','.'))
         self.current_total_timber=self.current_large_timber+self.current_small_timber
         self.corr_factor=self.get_corr_factor(stand)
-        self.large_timber_per_area=self.current_large_timber*self.corr_factor*stand['area']
-        self.small_timber_per_area=self.current_small_timber*self.corr_factor*stand['area']
+        self.large_timber_per_area=self.current_large_timber*self.corr_factor*self.area
+        self.small_timber_per_area=self.current_small_timber*self.corr_factor*self.area
         self.total_timber_per_area=self.large_timber_per_area+self.small_timber_per_area
-    def calculate_current_age(self,start_year,start_age,year):
-        return year-start_year+start_age
+        self.thinning_intensity=0.15
+        self.large_timber_share=0.8
+        self.small_timbar_share=1-self.large_timber_share
+        self.thinning_interval=10
+        self.thinning_start=40
+        self.harvest_age=100
+    def calculate_current_age(self,start_year,start_age):
+        return datetime.today().year-start_year+start_age
     def get_corr_factor(self,stand):
         yield_at_start=float(self.tablica.loc[self.tablica['Age']==stand['age']]['Total'].values[0].replace(',','.'))
         return stand['yield']/yield_at_start
+    def simulate(self,years):
+        simulated_age=self.current_age
+        simulated_total_volume=self.total_timber_per_area
+        simulation=[]
+        
+        
+        for i in range(years):
+            harvest=0
+            #regeneration
+            if(i+self.current_age==100):
+                simulated_age=0
+                #print("HARVESTING!")
+                harvest=simulated_total_volume
+            else:
+                if((simulated_age-self.thinning_start)%self.thinning_interval==0 and simulated_age>self.thinning_start and simulated_age!=self.harvest_age):
+                    #print("thinning")
+                    harvest=simulated_total_volume*self.thinning_intensity
+            #przyrost
+            try:
+                growth_rate=float(self.tablica.loc[self.tablica['Age']==simulated_age]['Increase'].values[0].replace(',','.'))*self.area
+            except:
+                print("Nie ma wartosci przyrostu w tablicy, uzywam 0")
+                growth_rate=0
+            
+            simulation.append([datetime.today().year+i,simulated_age,growth_rate,simulated_total_volume,harvest])
+            #update
+            simulated_age=1+simulated_age
+            simulated_total_volume=simulated_total_volume-harvest+growth_rate
+            #print(i+self.current_age-self.thinning_start)
+            
 
+            #print("Wiek:" +str(simulated_age)+" Rok: "+str(datetime.today().year+i))
+        df_simulation = pd.DataFrame(simulation, columns=['Year', 'Age', 'Growth rate','Total Volume','Harvested Timber'])    
+        title="Prognoza zabiegow dla adresu: "+self.address
+        df_simulation.plot(kind='line',x='Year',title=title)
+        plt.show()
     def print_summary(self):
         print('----------------------------------\n')
         print('Obecny wiek drzewostanu: '+str(self.current_age)+'\n')
@@ -60,11 +103,9 @@ class Stand():
 
 
 
-drzewostan_dzisiaj=Stand(dstan,datetime.today().year)
-drzewostan_dzisiaj.print_summary()
+drzewostan=Stand(dstan)
+#drzewostan.print_summary()
+drzewostan.simulate(100)
 #dstan za 15 lat
 #cos tutaj nie halo z tymi obliczeniami
-dstan_15=Stand(dstan,datetime.today().year+15)
-for x in range(15,100):
-    dstan_15=Stand(dstan,datetime.today().year+x)
-    print(dstan_15.total_timber_per_area)
+
